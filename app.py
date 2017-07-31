7971c090cfc0419fe7af10a2640929447d937c18
Matt Camack <mcamack@gmail.com>
2017-07-30 18:11:15 -0700

    adding html files to the flask program

--- app.py
+++ app.py
@@ -1,31 +1,31 @@
 #!flask/bin/python
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 from flask import abort, make_response
 
 app = Flask(__name__)
 
-tasks = [
-    {
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web', 
-        'done': False
-    }
-]
+tasks = [{
+    'id': 1,
+    'title': u'Buy groceries',
+    'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+    'done': False
+}, {
+    'id': 2,
+    'title': u'Learn Python',
+    'description': u'Need to find a good Python tutorial on the web',
+    'done': False
+}]
+
 
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
+
 @app.route('/todo/api/v1.0/tasks', methods=['GET'])
 def get_tasks():
     return jsonify({'tasks': tasks})
+
 
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
 def get_task(task_id):
@@ -34,6 +34,9 @@ def get_task(task_id):
         abort(404)
     return jsonify({'task': task[0]})
 
+@app.route('/html', methods=['GET'])
+def home():
+    return render_template('flask_mypage.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
-
